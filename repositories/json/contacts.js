5cd44016b6f230401707a42dd8268d83666f9f1c
Natalia <icxodnik@gmail.com>
2024-07-29 03:03:04 +0200

    fix path

--- repositories/json/contacts.js
+++ repositories/json/contacts.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs').promises;
 
-const contactsPath = path.join(__dirname, "db", "contacts.json");
+const contactsPath = path.join(__dirname, "..", "db", "contacts.json");
 
 async function update(data) {
     fs.writeFile(contactsPath, JSON.stringify(data, null, 2),
